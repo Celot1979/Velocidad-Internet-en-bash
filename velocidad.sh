a09eb3c74f168aaaf2f1f6d84e0b32d3e53af7fe
dani <zata_k@hotmail.com>
2024-08-01 10:33:56 +0200

    1 Agost 10:33 1ºCommit

--- velocidad.sh
+++ velocidad.sh
@@ -1,6 +1,49 @@
-
 #!/bin/bash
+echo "
+                                                           ############
+                                                           ################
+                                                                       ######
+                                                          #########       ####
+                                                         #############     ####
+                       ####                                       ######    ####
+                      #######                                        ####    ###
+                      ########                              ##        ####   ####
+                     ####  #####                          ########     ###    ###
+                    ####     #####                             ####    ####   ###
+                    ####       #####                  #######   ###     ###  ####
+                    ###          #####               #########  ###    ####  ###
+                   ####            #####             ###   ###         ###
+                   ###              ######           #########
+                   ###                #####        #########
+                   ###                  #####    #####
+                   ####                   ##########
+                    ###                     #######
+                    ###                       ####
+                    ####                       #####
+                     ###                         #####
+                      ###                          #####
+                      ####                           #####
+                       ####                            #####
+                        #####                           ######
+                          ####                            #####
+                           #####                            #####
+                             #####                            #####
+                               ######                           #####
+                              ##########                         #####
+                              ####  #########              #########
+                            ####        #########################
+                            ####            ##############
+                           ####              ###
+                          ####                ###
+                         ####                 ####
+                        ####                   ####
+                       ####                     ####
+                      ####                       ####
+                     ####                         ####
+                    ##################################
+                     ################################
 
+"
 # Función para mostrar una barra de progreso con calaveras grandes y porcentaje
 show_progress() {
     local duration=$1
@@ -25,11 +68,11 @@ show_progress() {
 }
 
 # Iniciar la barra de progreso en segundo plano
-show_progress 30 &  # Duración estimada de 30 segundos
+show_progress 50 &  # Duración estimada de 50 segundos
 PROGRESS_PID=$!
 
 # Medir la velocidad de Internet
-echo "Midiendo la velocidad de Internet..."
+echo "Realizando la medición........"
 result=$(speedtest-cli --json)
 
 # Detener la barra de progreso
@@ -45,20 +88,14 @@ SERVER_LOCATION=$(echo $result | jq -r '.server.location')
 SERVER_COUNTRY=$(echo $result | jq -r '.server.country')
 
 # Mostrar los resultados de forma vistosa
+echo -e "\n\033[1;34m-----------------------------------------------\033[0m"
 echo -e "\n\033[1;34mResultados de la prueba de velocidad de Internet:\033[0m"
-echo -e "-----------------------------------------------"
-echo -e "Latencia (Ping): \033[1;32m$PING ms\033[0m"
-echo -e "Velocidad de descarga: \033[1;32m$DOWNLOAD_SPEED Mbps\033[0m"
-echo -e "Velocidad de subida: \033[1;32m$UPLOAD_SPEED Mbps\033[0m"
-echo -e "Servidor: \033[1;32m$SERVER_NAME\033[0m"
-echo -e "Ubicación del servidor: \033[1;32m$SERVER_LOCATION, $SERVER_COUNTRY\033[0m"
-echo -e "-----------------------------------------------"
-
-# Despedida con arte ASCII "ADIOS"
-
-echo -e "\033[1;31m"
-echo -e "  _____   ____  _      ____  _____ \n / ____| / __ \\| |    / __ \\|  __ \\ \n| (___  | |  | | |   | |  | | |  | |\n \\___ \\ | |  | | |   | |  | | |  | |\n ____) || |__| | |___| |__| | |__| |\n|_____/  \\____/|______ \\____/ \\____/ \n                   |____/           "
-echo -e " \033[0m"
-
+echo -e "\033[1;34mLatencia (Ping):\033[0m  \033[1;32m$PING ms\033[0m"
+echo -e "\033[1;34mVelocidad de descarga:\033[0m  \033[1;32m$DOWNLOAD_SPEED Mbps\033[0m"
+echo -e "\033[1;34mVelocidad de subida:\033[0m  \033[1;32m$UPLOAD_SPEED Mbps\033[0m"
+echo -e "\033[1;34mServidor:\033[0m  \033[1;32m$SERVER_NAME\033[0m"
+echo -e "\033[1;34mUbicación del servidor:\033[0m  \033[1;32m$SERVER_LOCATION, $SERVER_COUNTRY\033[0m"
+echo -e "\n\033[1;34m-----------------------------------------------\033[0m"
+echo -e "\n\033[1;31m------------------------------------------------\033[1;31m-"
 # Línea del autor
-echo -e "\033[1;31m- Creado por: DANIEL GIL MARTINEZ \033[0m"
+echo -e "\033[1;31m- Autor: DANIEL GIL MARTINEZ \033[0m"
